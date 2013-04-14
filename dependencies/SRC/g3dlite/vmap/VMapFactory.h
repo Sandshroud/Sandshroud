4f803c51dc34517cc20660a8f778c28da365095b
Thetruecrow <Crow5736@yahoo.com>
2013-04-14 00:26:54 -0700

    Replacing tabs with spaces.

--- dependencies/SRC/g3dlite/vmap/VMapFactory.h
+++ dependencies/SRC/g3dlite/vmap/VMapFactory.h
@@ -10,16 +10,16 @@ This is the access point to the VMapManager.
 
 namespace VMAP
 {
-	//===========================================================
+    //===========================================================
 
-	class VMapFactory
-	{
-		public:
-			static VMapManager2* createOrGetVMapManager();
-			static void clear();
+    class VMapFactory
+    {
+        public:
+            static VMapManager2* createOrGetVMapManager();
+            static void clear();
 
-			static void preventSpellsFromBeingTestedForLoS(const char* pSpellIdString);
-			static bool checkSpellForLoS(unsigned int pSpellId);
-	};
+            static void preventSpellsFromBeingTestedForLoS(const char* pSpellIdString);
+            static bool checkSpellForLoS(unsigned int pSpellId);
+    };
 
 }
