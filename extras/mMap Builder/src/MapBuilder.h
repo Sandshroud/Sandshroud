eb3c7c3f5543e647d7eb1a38278717c25dac88dd
Thetruecrow <Crow5736@yahoo.com>
2013-04-17 02:08:32 -0700

    Updated extractors to use new libs.

--- extras/mMap Builder/src/MapBuilder.h
+++ extras/mMap Builder/src/MapBuilder.h
@@ -1,19 +1,19 @@
 /*
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _MAP_BUILDER_H
@@ -26,28 +26,26 @@
 #include "TerrainBuilder.h"
 #include "IntermediateValues.h"
 
-#include "WorldModel.h"
-
 #include "Recast.h"
 #include "DetourNavMesh.h"
 
-using namespace std;
 using namespace VMAP;
+
 // G3D namespace typedefs conflicts with ACE typedefs
 
 namespace MMAP
 {
-    typedef map<uint32,set<uint32>*> TileList;
+    typedef std::map<G3D::g3d_uint32, std::set<G3D::g3d_uint32>*> TileList;
     struct Tile
     {
         Tile() : chf(NULL), solid(NULL), cset(NULL), pmesh(NULL), dmesh(NULL) {}
         ~Tile()
         {
-            freeCompactHeightfield(chf);
-            freeContourSet(cset);
-            freeHeightField(solid);
-            freePolyMesh(pmesh);
-            freePolyMeshDetail(dmesh);
+            rcFreeCompactHeightfield(chf);
+            rcFreeContourSet(cset);
+            rcFreeHeightField(solid);
+            rcFreePolyMesh(pmesh);
+            rcFreePolyMeshDetail(dmesh);
         }
         rcCompactHeightfield* chf;
         rcHeightfield* solid;
@@ -60,52 +58,52 @@ namespace MMAP
     {
         public:
             MapBuilder(float maxWalkableAngle   = 60.f,
-                       bool skipLiquid          = false,
-                       bool skipContinents      = false,
-                       bool skipJunkMaps        = true,
-                       bool skipBattlegrounds   = false,
-                       bool debugOutput         = false,
-                       bool bigBaseUnit         = false,
-                       const char* offMeshFilePath = NULL);
+                bool skipLiquid          = false,
+                bool skipContinents      = false,
+                bool skipJunkMaps        = true,
+                bool skipBattlegrounds   = false,
+                bool debugOutput         = false,
+                bool bigBaseUnit         = false,
+                const char* offMeshFilePath = NULL);
 
             ~MapBuilder();
 
             // builds all mmap tiles for the specified map id (ignores skip settings)
-            void buildMap(uint32 mapID);
+            void buildMap(G3D::g3d_uint32 mapID);
+            void buildMeshFromFile(char* name);
 
             // builds an mmap tile for the specified map and its mesh
-            void buildSingleTile(uint32 mapID, uint32 tileX, uint32 tileY);
+            void buildSingleTile(G3D::g3d_uint32 mapID, G3D::g3d_uint32 tileX, G3D::g3d_uint32 tileY);
 
             // builds list of maps, then builds all of mmap tiles (based on the skip settings)
-            void buildAllMaps();
+            void buildAllMaps(int threads);
 
         private:
             // detect maps and tiles
             void discoverTiles();
-            set<uint32>* getTileList(uint32 mapID);
+            std::set<G3D::g3d_uint32>* getTileList(G3D::g3d_uint32 mapID);
 
-            void buildNavMesh(uint32 mapID, dtNavMesh* &navMesh);
+            void buildNavMesh(G3D::g3d_uint32 mapID, dtNavMesh* &navMesh);
 
-            void buildTile(uint32 mapID, uint32 tileX, uint32 tileY, dtNavMesh* navMesh, uint32 index);
+            void buildTile(G3D::g3d_uint32 mapID, G3D::g3d_uint32 tileX, G3D::g3d_uint32 tileY, dtNavMesh* navMesh);
 
             // move map building
-            void buildMoveMapTile(uint32 mapID,
-                                  uint32 tileX,
-                                  uint32 tileY,
-                                  MeshData &meshData,
-                                  float bmin[3],
-                                  float bmax[3],
-                                  dtNavMesh* navMesh,
-								  uint32 index);
+            void buildMoveMapTile(G3D::g3d_uint32 mapID,
+                G3D::g3d_uint32 tileX,
+                G3D::g3d_uint32 tileY,
+                MeshData &meshData,
+                float bmin[3],
+                float bmax[3],
+                dtNavMesh* navMesh);
 
-            void getTileBounds(uint32 tileX, uint32 tileY,
-                               float* verts, int vertCount,
-                               float* bmin, float* bmax);
-            void getGridBounds(uint32 mapID, uint32 &minX, uint32 &minY, uint32 &maxX, uint32 &maxY);
+            void getTileBounds(G3D::g3d_uint32 tileX, G3D::g3d_uint32 tileY,
+                float* verts, int vertCount,
+                float* bmin, float* bmax);
+            void getGridBounds(G3D::g3d_uint32 mapID, G3D::g3d_uint32 &minX, G3D::g3d_uint32 &minY, G3D::g3d_uint32 &maxX, G3D::g3d_uint32 &maxY);
 
-            bool shouldSkipMap(uint32 mapID);
-            bool isTransportMap(uint32 mapID);
-            bool shouldSkipTile(uint32 mapID, uint32 tileX, uint32 tileY);
+            bool shouldSkipMap(G3D::g3d_uint32 mapID);
+            bool isTransportMap(G3D::g3d_uint32 mapID);
+            bool shouldSkipTile(G3D::g3d_uint32 mapID, G3D::g3d_uint32 tileX, G3D::g3d_uint32 tileY);
 
             TerrainBuilder* m_terrainBuilder;
             TileList m_tiles;
@@ -122,6 +120,21 @@ namespace MMAP
 
             // build performance - not really used for now
             rcContext* m_rcContext;
+    };
+
+    class MapBuildRequest
+    {
+        public:
+            MapBuildRequest(G3D::g3d_uint32 mapId) : _mapId(mapId) {}
+
+            virtual int call()
+            {
+                /// @ Actually a creative way of unabstracting the class and returning a member variable
+                return (int)_mapId;
+            }
+
+        private:
+            G3D::g3d_uint32 _mapId;
     };
 }
 
