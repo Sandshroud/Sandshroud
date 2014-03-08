2d924d51956ea8c029723336300821bb7ac9d9da
Thetruecrow <Crow5736@yahoo.com>
2014-03-08 07:42:20 -0800

    Dropping item commands.

--- src/hearthstone-world/Mainframe/World.h
+++ src/hearthstone-world/Mainframe/World.h
@@ -681,7 +681,6 @@ public:
     void QueueShutdown(uint32 delay, uint32 type);
     void CancelShutdown();
     void UpdateShutdownStatus();
-    void UpdatePlayerItemInfos();
 
     bool VerifyName(const char * name, size_t nlen)
     {
